7eeafce2f049b96fdf9d03c2d92947c8f8dadd60
Dwayne Thomas <WizHome@Dwaynes-Air.fios-router.home>
2019-05-20 12:48:47 -0400

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+var CardOne = "queen";
+var CardTwo = "king";
+var CardThree = "queen";
+var CardFour = "king";
+
+console.log("user flipped " + CardOne);
+console.log("user flipped " + CardTwo)
