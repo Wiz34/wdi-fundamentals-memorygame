7b19851bd17ac2213d2ffaef5c33ac5b9396dba6
Dwayne Thomas <WizHome@Dwaynes-Air.fios-router.home>
2019-05-22 20:03:37 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,22 @@
-console.log("Up and running!");
 
-var CardOne = "queen";
-var CardTwo = "king";
-var CardThree = "queen";
-var CardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay =[];
 
-console.log("user flipped " + CardOne);
-console.log("user flipped " + CardTwo)
+var cardOne;
+cards[2] = cardOne;
+
+cardsInPlay.push('cardOne');
+console.log("User flipped queen");
+
+var cardTwo;
+cards[2] = cardTwo;
+
+cardsInPlay.push('cardTwo');
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2 ){
+	console.log(alert("You found a match!"));
+} else if (cardsInPlay[0] === cardsInPlay[1]);{
+		console.log(alert("sorry, try again"));
+}
+
